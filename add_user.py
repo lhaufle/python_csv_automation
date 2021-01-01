d16d19b3135aac7fa638e882b90d2e2df9910c16
lhaufle <ihaufle123@gmail.com>
2021-01-01 10:19:20 -0600

    Update add_user.py

--- add_user.py
+++ add_user.py
@@ -1,3 +1,10 @@
+'''
+The AddUser class takes zero to three arguments in the custructor. 
+A dictionary is added to a list. Prior values from the csv document, 
+if any, will also be appended to list as dictionaries to be used by
+the read_from_csv() method that is meant to be used to pass the values
+to the Word_Gen class as an argument. 
+'''
 import csv
 import os.path
 
@@ -5,8 +12,12 @@ class AddUser:
 
     field_names = ['user_name', 'address', 'email']
 
+    #defaults sent to None in constructor to allow for zero parameters args
     def __init__(self, name = None, address = None, email = None):
-        self.users = [{'user_name': name, 'address': address, 'email': email}]
+        if name == None and address == None and email == None:
+            self.users = []
+        else:
+            self.users = [{'user_name': name, 'address': address, 'email': email}]
 
     def read_from_csv(self):
         user_list = []
